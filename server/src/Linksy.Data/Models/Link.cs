86f1e80444d28cd70e10927dd82001310f177cd9
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:26:07 +0300

    Created Link entity.

---
+++ server/src/Linksy.Data/Models/Link.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Linksy.Data.Models;
+
+public class Link
+{
+    public Guid Id { get; set; }
+
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
+
+    public string ShortCode { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+    public bool IsActive { get; set; }
+}
