cf35e81ef436da1bb3d998027943e7c5e8f13a96
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:55:37 +0300

    Added static EntityValidation class with rules for entity properties.

---
+++ server/src/Linksy.Data/Common/EntityValidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Common;
+
+public static class EntityValidation
+{
+    public static class ApplicationUser
+    {
+        public const int UserNameMinLength = 3;
+        public const int UserNameMaxLength = 30;
+        public const string UserNamePattern = "^[a-zA-Z0-9_]+$";
+
+        public const int EmailMaxLength = 254;
+    }
+
+    public static class Link
+    {
+        public const int OriginalUrlMaxLength = 2048;
+        public const int ShortCodeMinLength = 3;
+        public const int ShortCodeMaxLength = 20;
+        public const string ShortCodePattern = "^[a-zA-Z0-9-]+$";
+    }
+
+    public static class Click
+    {
+        public const int IpAddressMaxLength = 45;
+        public const int UserAgentMaxLength = 512;
+        public const int RefererMaxLength = 2048;   
+    }
+}
