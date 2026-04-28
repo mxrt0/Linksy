a4e6e128f09259615cc9de9a43ed3c34ca61c0d9
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:36:26 +0300

    Created Click entity.

---
+++ server/src/Linksy.Data/Models/Click.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Linksy.Data.Models;
+
+public class Click
+{
+    public Guid Id { get; set; }
+    
+    [ForeignKey(nameof(Link))]
+    public Guid LinkId { get; set; }
+    public Link Link { get; set; } = null!;
+
+    public DateTime ClickedAt { get; set; } = DateTime.UtcNow;
+    public string IpAddress { get; set; } = null!;
+    public string UserAgent { get; set; } = null!;
+    public string Referer { get; set; } = null!;
+}
