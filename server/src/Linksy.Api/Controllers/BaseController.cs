64005306369505ce32bf77d048739b311c6b9598
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:11:05 +0300

    Added base API controller with method for getting user ID

---
+++ server/src/Linksy.Api/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Linksy.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected string? GetUserId() => User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+}
