2721dda15c403f8be03193850615a02c13784298
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:57:51 +0300

    Added user entity configuration class.

---
+++ server/src/Linksy.Data/Configuration/ApplicationUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Linksy.Data.Models;
+using static Linksy.Data.Common.EntityValidation.ApplicationUser;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Configuration;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder
+            .Property(u => u.UserName)
+            .HasMaxLength(UserNameMaxLength);
+    }
+}
