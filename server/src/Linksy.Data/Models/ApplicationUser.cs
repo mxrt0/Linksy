bc38e994a8681bd04a18e4a35150ef298931f397
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:22:54 +0300

    Created ApplicationUser entity

---
+++ server/src/Linksy.Data/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
