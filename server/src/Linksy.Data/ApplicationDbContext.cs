9017dd4a4ee1b3ce68bcd9c2a604e0ea0fa84c50
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 17:39:48 +0300

    Created DbContext.

---
+++ server/src/Linksy.Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Linksy.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    public DbSet<Link> Links { get; set; } = null!;
+    public DbSet<Click> Clicks { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(builder);
+    }
+}
