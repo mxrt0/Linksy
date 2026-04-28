0d3594ad26082d7878cf1328ae7bd271c0eee848
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 18:04:30 +0300

    Added click entity configuration.

---
+++ server/src/Linksy.Data/Configuration/ClickConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Linksy.Data.Models;
+using static Linksy.Data.Common.EntityValidation.Click;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Configuration;
+
+public class ClickConfiguration : IEntityTypeConfiguration<Click>
+{
+    public void Configure(EntityTypeBuilder<Click> builder)
+    {
+        builder
+            .Property(c => c.IpAddress)
+            .HasMaxLength(IpAddressMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(c => c.UserAgent)
+            .HasMaxLength(UserAgentMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(c => c.Referer)
+            .HasMaxLength(RefererMaxLength)
+            .IsRequired();
+    }
+}
