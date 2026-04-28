91e74543f12ca81f19840faaffff1e27c7e34d01
Martin Terziev <m.terziev08@abv.bg>
2026-04-28 18:02:11 +0300

    Added link entity configuration.

---
+++ server/src/Linksy.Data/Configuration/LinkConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Linksy.Data.Models;
+using static Linksy.Data.Common.EntityValidation.Link;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Configuration;
+
+public class LinkConfiguration : IEntityTypeConfiguration<Link>
+{
+    public void Configure(EntityTypeBuilder<Link> builder)
+    {
+        builder
+            .Property(l => l.OriginalUrl)
+            .HasMaxLength(OriginalUrlMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(l => l.ShortCode)
+            .HasMaxLength(ShortCodeMaxLength)
+            .IsRequired();  
+        
+
+    }
+}
