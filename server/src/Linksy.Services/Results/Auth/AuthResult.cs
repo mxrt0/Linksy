5ec5415f0f28e7a18e2bf9c69968cd0dc6ccbae1
Martin Terziev <m.terziev08@abv.bg>
2026-04-29 18:56:31 +0300

    Created AuthResult wrapper for service methods.

---
+++ server/src/Linksy.Services/Results/Auth/AuthResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.Results.Auth;
+
+public class AuthResult
+{
+    public bool Succeeded { get; set; }
+    public IEnumerable<string> Errors { get; set; } = Enumerable.Empty<string>();
+    public string? UserId { get; set; }
+}
