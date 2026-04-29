6517c2f6f5fe8ad3c58c512cde9339805e27eb76
Martin Terziev <m.terziev08@abv.bg>
2026-04-29 18:56:16 +0300

    Created register request DTO

---
+++ server/src/Linksy.Services/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.DTOs.Auth;
+
+public sealed record RegisterRequest(string Username, string Email, string Password);
