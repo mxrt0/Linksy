390f46f95570a498fcf6a0af710dacaf1cd272e9
Martin Terziev <m.terziev08@abv.bg>
2026-04-29 19:06:41 +0300

    Added username to auth result wrapper.

--- server/src/Linksy.Services/Results/Auth/AuthResult.cs
+++ server/src/Linksy.Services/Results/Auth/AuthResult.cs
@@ -9,4 +9,5 @@ public class AuthResult
     public bool Succeeded { get; set; }
     public IEnumerable<string> Errors { get; set; } = Enumerable.Empty<string>();
     public string? UserId { get; set; }
+    public string? Username { get; set; }
 }
