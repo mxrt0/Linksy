1df642b174aa8421dc6fee3f03e28ede81709b33
Martin Terziev <m.terziev08@abv.bg>
2026-04-29 18:56:51 +0300

    Implemented auth service with register method.

---
+++ server/src/Linksy.Services/Core/AuthService.cs
@@ -0,0 +1,37 @@
+﻿using Linksy.Data.Models;
+using Linksy.Services.Core.Contracts;
+using Linksy.Services.DTOs.Auth;
+using Linksy.Services.Results.Auth;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.Core;
+
+public class AuthService(UserManager<ApplicationUser> userManager) : IAuthService
+{
+    public async Task<ApplicationUser?> GetUserByNameAsync(string username)
+    {
+        return await userManager.FindByNameAsync(username);
+    }
+
+    public async Task<AuthResult> RegisterAsync(RegisterRequest request)
+    {
+       var user = new ApplicationUser
+        {
+            UserName = request.Username,
+            Email = request.Email,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var result = await userManager.CreateAsync(user, request.Password);
+
+        return new AuthResult
+        {
+            Succeeded = result.Succeeded,
+            UserId = result.Succeeded ? user.Id : null,
+            Errors = result.Errors.Select(e => e.Description)
+        };
+    }
+}
---
+++ server/src/Linksy.Services/Core/Contracts/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Linksy.Data.Models;
+using Linksy.Services.DTOs.Auth;
+using Linksy.Services.Results.Auth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.Core.Contracts;
+
+public interface IAuthService
+{
+    Task<AuthResult> RegisterAsync(RegisterRequest request);
+    Task<ApplicationUser?> GetUserByNameAsync(string username);
+}
