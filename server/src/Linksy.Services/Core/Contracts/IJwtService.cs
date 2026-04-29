142075d3895ed417ff2de636bb7f2ee73f3e9ccf
Martin Terziev <m.terziev08@abv.bg>
2026-04-29 18:57:13 +0300

    Implemented JWT service with generation method.

---
+++ server/src/Linksy.Services/Core/Contracts/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.Core.Contracts;
+
+public interface IJwtService
+{
+    string GenerateToken(string userId, string username);
+}
---
+++ server/src/Linksy.Services/Core/JwtService.cs
@@ -0,0 +1,36 @@
+﻿using Linksy.Services.Core.Contracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Linksy.Services.Core;
+
+public class JwtService(IConfiguration config) : IJwtService
+{
+    public string GenerateToken(string userId, string username)
+    {
+        var jwtSettings = config.GetSection("Jwt");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub,   userId),
+            new Claim(JwtRegisteredClaimNames.UniqueName, username)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(jwtSettings.GetValue<double>("ExpiryMinutes")),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
