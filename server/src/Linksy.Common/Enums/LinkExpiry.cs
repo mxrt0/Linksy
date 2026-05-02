ef2cbb7f39c02b91ca80aede2759441b335f980d
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 18:16:53 +0300

    Added enum map for link expiry options.

---
+++ server/src/Linksy.Common/Enums/LinkExpiry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Common.Enums;
+
+public enum LinkExpiry
+{
+    Never,
+    OneDay,
+    SevenDays,
+    ThirtyDays
+}
