48f5bab1ed0a36f969c4a49add1e40859b0e7fdb
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 19:51:18 +0300

    Implemented links controller with create link action.

---
+++ server/src/Linksy.Api/Controllers/LinksController.cs
@@ -0,0 +1,30 @@
+﻿using Linksy.Services.Core.Contracts;
+using Linksy.Services.DTOs.Link;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linksy.Api.Controllers;
+
+[Authorize]
+public class LinksController(ILinkService linkService) : BaseController
+{
+    [HttpPost]
+    public async Task<ActionResult<LinkDto>> CreateLink(CreateLinkRequest request)
+    {
+        var userId = GetUserId();
+        if (userId is null)
+        {
+            return Unauthorized();
+        }
+
+        var result = await linkService.CreateLinkAsync(request, userId);
+
+        if (!result.Success)
+        {
+            return BadRequest(result);
+        }
+
+        return Ok(result.Data);
+    }
+}
