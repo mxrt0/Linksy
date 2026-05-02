97faba31f4950e33c8293a0c556d8bd8a658de73
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 18:24:09 +0300

    Implemented link repository with add method.

---
+++ server/src/Linksy.Data/Repositories/Contracts/ILinkRepository.cs
@@ -0,0 +1,11 @@
+﻿using Linksy.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Repositories.Contracts;
+
+public interface ILinkRepository
+{
+    Task AddAsync(Link link);
+}
---
+++ server/src/Linksy.Data/Repositories/LinkRepository.cs
@@ -0,0 +1,16 @@
+﻿using Linksy.Data.Models;
+using Linksy.Data.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Data.Repositories;
+
+public class LinkRepository(ApplicationDbContext context) : ILinkRepository
+{
+    public async Task AddAsync(Link link)
+    {
+        await context.Links.AddAsync(link);
+        await context.SaveChangesAsync();
+    }
+}
