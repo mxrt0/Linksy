d392bfdcc800d1d4a4df3ffc4be58cb3af0adac4
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 19:50:58 +0300

    Changed service result property name from 'errorMessage' to 'error'

--- server/src/Linksy.Services/Results/ServiceResult.cs
+++ server/src/Linksy.Services/Results/ServiceResult.cs
@@ -7,11 +7,11 @@ namespace Linksy.Services.Results;
 public class ServiceResult<T>
 {
     public bool Success { get; private set; }
-    public string? ErrorMessage { get; private set; }
+    public string? Error { get; private set; }
     public T? Data { get; private set; }
 
     public static ServiceResult<T> Ok(T data)
         => new ServiceResult<T> { Success = true, Data = data };
     public static ServiceResult<T> Fail(string message)
-        => new ServiceResult<T> { Success = false, ErrorMessage = message };
+        => new ServiceResult<T> { Success = false, Error = message };
 }
