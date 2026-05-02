ea1d509b32fb2509d15274d5aa74804ba57caeaf
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 18:34:10 +0300

    Added url validation in dto.

--- server/src/Linksy.Services/DTOs/Link/CreateLinkRequest.cs
+++ server/src/Linksy.Services/DTOs/Link/CreateLinkRequest.cs
@@ -1,12 +1,14 @@
 ﻿using Linksy.Common.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Linksy.Services.DTOs.Link;
 
 public class CreateLinkRequest
 {
+    [Url]
     public string OriginalUrl { get; set; } = null!;
     public string ShortCode { get; set; } = null!;
     public LinkExpiry Expiry { get; set; }  
