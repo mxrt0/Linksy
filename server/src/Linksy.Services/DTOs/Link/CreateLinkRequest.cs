5d29a0c13987371600eb9886d37aeb300d1709d4
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 18:18:11 +0300

    Created a DTO for link creation requests.

---
+++ server/src/Linksy.Services/DTOs/Link/CreateLinkRequest.cs
@@ -0,0 +1,13 @@
+﻿using Linksy.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.DTOs.Link;
+
+public class CreateLinkRequest
+{
+    public string OriginalUrl { get; set; } = null!;
+    public string ShortCode { get; set; } = null!;
+    public LinkExpiry Expiry { get; set; }  
+}
