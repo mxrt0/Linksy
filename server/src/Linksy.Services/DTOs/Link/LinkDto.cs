d336ea8f5c4ac3a81ff9c12fcd5cc17ab6e00721
Martin Terziev <m.terziev08@abv.bg>
2026-05-02 18:32:02 +0300

    Created LinkDto.

---
+++ server/src/Linksy.Services/DTOs/Link/LinkDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.DTOs.Link;
+
+internal class LinkDto
+{
+    public string ShortCode { get; set; } = null!;
+    public string OriginalUrl { get; set; } = null!;
+    public string ShortUrl { get; set; } = null!;
+}
