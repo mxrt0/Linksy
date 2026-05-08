e153a0fbd174a04e8f510d7f318b42ad3e18b550
Martin Terziev <m.terziev08@abv.bg>
2026-05-08 21:35:57 +0300

    Created LInk Analytics DTO

---
+++ server/src/Linksy.Services/DTOs/Link/LinkAnalyticsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linksy.Services.DTOs.Link;
+
+public class LinkAnalyticsDto
+{
+    public Guid LinkId { get; set; }
+    public int TotalClicks { get; set; }
+    public List<DailyClicksDto> ClicksByDay { get; set; } = [];
+}
+
+public class DailyClicksDto
+{
+    public DateTime Date { get; set; }
+    public int Count { get; set; }
+}
