8a63e1c43c6f508ffc186a51dc83abbc6f525f7d
Martin Terziev <m.terziev08@abv.bg>
2026-05-08 21:37:09 +0300

    Created analytics controller and implemented simple endpoint for getting link analytics.

---
+++ server/src/Linksy.Api/Controllers/AnalyticsController.cs
@@ -0,0 +1,21 @@
+﻿using Linksy.Services.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linksy.Api.Controllers;
+
+public class AnalyticsController(IAnalyticsService analyticsService) : BaseController
+{
+    [HttpGet("{linkId:guid}")]
+    public async Task<ActionResult> GetLinkAnalytics(Guid linkId)
+    {
+        var result = await analyticsService.GetLinkAnalyticsAsync(linkId);  
+
+        if (!result.Success)
+        {
+            return NotFound(result);
+        }
+
+        return Ok(result.Data);  
+    }
+}
