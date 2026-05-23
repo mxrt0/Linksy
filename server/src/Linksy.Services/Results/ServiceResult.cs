7a536977d9204fb3473d6fbeebb373bd37c94a2c
Martin Terziev <m.terziev08@abv.bg>
2026-05-23 12:20:02 +0300

    Added new static method with suggestions for failed service result.

--- server/src/Linksy.Services/Results/ServiceResult.cs
+++ server/src/Linksy.Services/Results/ServiceResult.cs
@@ -13,6 +13,13 @@ public class ServiceResult<T> : ServiceResult
         => new ServiceResult<T> { Success = true, Data = data };
     public static new ServiceResult<T> Fail(string message)
         => new ServiceResult<T> { Success = false, Error = message };
+    public static new ServiceResult<T> Fail(string message, List<string>? suggestions)
+        => new()
+        {
+            Success = false,
+            Error = message,
+            Suggestions = suggestions
+        };
 }
 
 public class ServiceResult
@@ -21,8 +28,16 @@ public class ServiceResult
 
     public bool Success { get; protected init; }
     public string? Error { get; protected init; }
+    public List<string>? Suggestions { get; protected init; }
     public static ServiceResult Ok()
         => new ServiceResult { Success = true };
     public static ServiceResult Fail(string message)
         => new ServiceResult { Success = false, Error = message };
+    public static ServiceResult Fail(string message, List<string>? suggestions)
+       => new()
+       {
+           Success = false,
+           Error = message,
+           Suggestions = suggestions
+       };
 }
